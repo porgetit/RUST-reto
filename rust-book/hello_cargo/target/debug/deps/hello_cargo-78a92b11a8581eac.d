adc79dd2f304352fe3c83d2b6704ee649210c0d1
Porgetit <kevin.esguerra@utp.edu.co>
2024-12-06 00:39:34 -0500

    Segundo commit

---
+++ rust-book/hello_cargo/target/debug/deps/hello_cargo-78a92b11a8581eac.d
@@ -0,0 +1,5 @@
+/home/kevin/RUST-reto/rust-book/hello_cargo/target/debug/deps/libhello_cargo-78a92b11a8581eac.rmeta: src/main.rs
+
+/home/kevin/RUST-reto/rust-book/hello_cargo/target/debug/deps/hello_cargo-78a92b11a8581eac.d: src/main.rs
+
+src/main.rs:
---
+++ rust-book/hello_cargo/target/debug/deps/hello_cargo-ecd8def3cace2029.d
@@ -0,0 +1,5 @@
+/home/kevin/RUST-reto/rust-book/hello_cargo/target/debug/deps/libhello_cargo-ecd8def3cace2029.rmeta: src/main.rs
+
+/home/kevin/RUST-reto/rust-book/hello_cargo/target/debug/deps/hello_cargo-ecd8def3cace2029.d: src/main.rs
+
+src/main.rs:
